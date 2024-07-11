b05e4867f8b19070cdfb61f9e228b9080bbba898
Amir Rezaii <43274376+AmirRezaii@users.noreply.github.com>
2024-07-11 10:19:34 +0330

    1.0

--- src/main.php
+++ src/main.php
@@ -4,6 +4,12 @@ use TBot\Bot;
 
 $bot = new Bot($_ENV["telegram_token"]);
 
+$bot->default = [
+    "sendMessage" => [
+        "chat_id" => $_ENV["admin_id"]
+    ]
+];
+
 $bot->getUpdate();
 $bot->setWebhook("https://voidtree.ir/voidtreebot");
 $bot->sendMessage($bot->result);
